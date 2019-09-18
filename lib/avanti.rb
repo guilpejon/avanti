a11ac7bce3e925ec98f29125a2b537be7729ea5a
Guilherme Pejon <guilpejon@gmail.com>
2019-09-17 21:35:45 -0300

    Initial commit

---
+++ lib/avanti.rb
@@ -0,0 +1,6 @@
+require "avanti/version"
+
+module Avanti
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/avanti_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Avanti do
+  it "has a version number" do
+    expect(Avanti::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
