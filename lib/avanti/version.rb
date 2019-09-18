249c61622952de331a65cec5400ac7ecbee30239
Guilherme Pejon <guilpejon@gmail.com>
2019-09-17 21:50:10 -0300

    Added rails and ruby versions

--- lib/avanti/version.rb
+++ lib/avanti/version.rb
@@ -1,3 +1,8 @@
 module Avanti
   VERSION = "0.1.0"
+  RAILS_VERSION = "~> 6.0.0".freeze
+  RUBY_VERSION = IO.
+    read("#{File.dirname(__FILE__)}/../../.ruby-version").
+    strip.
+    freeze
 end
